b7c8c33641ff9471aff82201c941ea3a06ef5740
jozephbautista <jozeph.bautista@seattle.mantistgi.com>
2016-08-17 12:58:23 -0700

    Add schema for discharge-admissions and discharge-patient

--- src/UI/patient-risk-api/app/models/discharge-admissions.js
+++ src/UI/patient-risk-api/app/models/discharge-admissions.js
@@ -7,8 +7,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dischargeAdmissionsSchema = new Schema({
-  name: String,
-  age: Number
+  row_id: Number,
+  subject_id: Number,
+  hadm_id: Number,
+  admittime: Date,
+  dischtime: Date,
+  deathtime: Date,
+  admission_type: String,
+  admission_location: String,
+  discharge_location: String,
+  insurance: String,
+  language: String,
+  religion: String,
+  marital_status: String,
+  ethnicity: String,
+  edregtime: Date,
+  edouttime: Date,
+  diagnosis: String,
+  hospital_expire_flag: Number,
+  has_ioevents_data: Number,
+  has_chartevents_data: Number
 });
 
 module.exports = mongoose.model('DischargeAdmission', dischargeAdmissionsSchema);
