f1c8a4496d6234e61ba2f8ffb96c2fc259921376
jozephbautista <jozeph.bautista@seattle.mantistgi.com>
2016-08-19 16:24:01 -0700

    POC for ng2-table

--- src/UI/patient-risk/src/main.ts
+++ src/UI/patient-risk/src/main.ts
@@ -1,9 +1,15 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+// import { CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgIf } from '@angular/common';
+// import { PAGINATION_DIRECTIVES } from 'ng2-bootstrap';
+// import { NG_TABLE_DIRECTIVES } from 'ng2-table';
+// import { TableData } from './table-data';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent);
+
+
