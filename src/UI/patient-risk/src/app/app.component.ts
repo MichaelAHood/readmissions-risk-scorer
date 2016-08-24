6131bd20454febc494ca720c65d890a5595009f2
jozephbautista <jozeph.bautista@seattle.mantistgi.com>
2016-08-24 15:34:39 -0700

    Initial layout for readmission-risk-results page

--- src/UI/patient-risk/src/app/app.component.ts
+++ src/UI/patient-risk/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header';
 import { PatientSelectComponent } from './patient-select';
+import { ReadmissionRiskResultsComponent} from './readmission-risk-results';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  directives: [HeaderComponent, PatientSelectComponent]
+  directives: [HeaderComponent, PatientSelectComponent, ReadmissionRiskResultsComponent]
 })
 export class AppComponent {
 }
--- src/UI/patient-risk/src/app/readmission-risk-results/readmission-risk-results.component.ts
+++ src/UI/patient-risk/src/app/readmission-risk-results/readmission-risk-results.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'pk-readmission-risk-results',
-  templateUrl: 'readmission-risk-results.component.html'
+  templateUrl: 'readmission-risk-results.component.html',
+  styleUrls: ['readmission-risk-results.css']
 })
 export class ReadmissionRiskResultsComponent implements OnInit {
+
+  public comorbiditySeverity:string = "0";
+  public comorbidityMorality:string = "0";
+  public age:string = "0";
+  public riskscore:string = "0";
 
   constructor() { }
 
