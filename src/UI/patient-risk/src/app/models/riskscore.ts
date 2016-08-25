bf0153b6cd1acf6308aa5c793a01c7eba6fb8e94
André Davis <cubicle.jockey@live.com>
2016-08-25 10:46:28 -0700

    Setting up service to call the Risk Score calculator
    I am stuck because the api isn't returning cross-orgins information in
    the header of the response.

---
+++ src/UI/patient-risk/src/app/models/riskscore.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by prokarma on 8/25/2016.
+ */
+export class RiskScore{
+  hadm_id: number;
+  risk_score: number;
+}
--- src/UI/patient-risk/src/app/readmission-risk-results/readmission-risk-results.component.ts
+++ src/UI/patient-risk/src/app/readmission-risk-results/readmission-risk-results.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientService } from '../services';
 
 @Component({
   moduleId: module.id,
   selector: 'pk-readmission-risk-results',
   templateUrl: 'readmission-risk-results.component.html',
-  styleUrls: ['readmission-risk-results.css']
+  styleUrls: ['readmission-risk-results.css'],
+  providers: [PatientService]
 })
 export class ReadmissionRiskResultsComponent implements OnInit {
 
-  public comorbiditySeverity:string = "0";
-  public comorbidityMorality:string = "0";
-  public age:string = "0";
-  public riskscore:string = "0";
+  private comorbiditySeverity: string = "0";
+  private comorbidityMorality: string = "0";
+  private age: string = "0";
+  private riskscore: string = "0";
+  private errorMessage: string;
 
-  constructor() { }
+
+  constructor(private patientService: PatientService) { }
 
   ngOnInit() {
+  /*  this.patientService.getRiskScores([165315]).subscribe(
+      rs => {},
+      e => this.errorMessage = e
+    );*/
   }
 
 }
