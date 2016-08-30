0c5e8cd44b1c91f39071be555576e92097fe021b
André Davis <cubicle.jockey@live.com>
2016-08-30 10:58:37 -0700

    Basic Structure to place the filtering of the data where it needs to go.

--- src/UI/patient-risk/src/app/patient-select/index.ts
+++ src/UI/patient-risk/src/app/patient-select/index.ts
@@ -1,5 +1,4 @@
 export { PatientSelectComponent } from './patient-select.component';
 export { DischargePopulationComponent } from './discharge-population.component';
-export { FilterPopulationByComponent } from './filter-population-by.component';
 export { ProportionAtRiskComponent } from './proportion-at-risk.component';
 export { DischargePopulation2Component } from './discharge-population2.component';
