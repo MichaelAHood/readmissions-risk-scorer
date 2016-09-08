d2299ac07a4952d2d5b8eb10414bee906b08926d
Michael Hood <hoodmichaelallen@gmail.com>
2016-09-08 16:26:53 -0700

    updated server.py to cast ages string to int

--- src/data-APIs/reference-data/server.py
+++ src/data-APIs/reference-data/server.py
@@ -17,25 +17,27 @@ app = Flask(__name__)
 # When running this app on the local machine, default the port to 8080
 port = int(os.getenv('VCAP_APP_PORT', 8080))
 
-referenceDF = pd.read_csv('app-reference-data.csv')
-readmissionDF = pd.read_csv('app-readmission-data.csv')
+referenceDF = pd.read_csv('data/app-reference-data.csv')
+readmissionDF = pd.read_csv('data/app-readmission-data.csv')
 
 def get_data(df, age):
-    if (age < 25) & (age >= 1):
+    patientAge = int(age)
+    print patientAge
+    if (patientAge < 25) & (patientAge >= 1):
         return df[(df.age < 25) & (df.age >= 1)]
-    elif (age >= 25) & (age < 50):
+    elif (patientAge >= 25) & (patientAge < 50):
         return df[(df.age >= 25) & (df.age < 50)]
     return df[df.age >= 50]
 
 def convert_reference_to_json(df):
-    referenceResult = {'age': list(np.round(df.age.values, 2)),
-                       'comorbid_severity': list(np.round(df.avg_severity.values, 2)),
-                       'comorbid_mortality': list(np.round(df.avg_mortality.values, 2))}
+    referenceResult = {'ages': list(np.round(df.age.values, 2)),
+                       'comorbid_severities': list(np.round(df.avg_severity.values, 2)),
+                       'comorbid_mortalities': list(np.round(df.avg_mortality.values, 2))}
     return json.dumps(referenceResult)
 
 def convert_readmission_to_json(df):
-    readmissionResult = {'readmissionRate': list(df.readmissionRate.values),
-                         'date': list(df.date.values)}
+    readmissionResult = {'readmissionRates': list(df.readmissionRate.values),
+                         'dates': list(df.date.values)}
     return json.dumps(readmissionResult)
 ########################################################################################################################
 # Routes
@@ -49,15 +51,23 @@ def root():
 
 @app.route('/v1/get-reference-data', methods=['GET'])
 def parse_qs():
-    patientAge = request.args.get('patientAge')
+    patientAge = request.args.get('ages')
     patientReferenceDF = get_data(referenceDF, patientAge)
     jsonResponse = convert_reference_to_json(patientReferenceDF)
-    return Response(jsonResponse, mimetype='text/plain')
+    response = Response(jsonResponse, mimetype='application/json')
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
+    return response
 
 @app.route('/v1/get-readmission-data', methods=['GET'])
 def return_qs():
     jsonResponse = convert_readmission_to_json(readmissionDF)
-    return Response(jsonResponse, mimetype='text/plain')
+    response = Response(jsonResponse, mimetype='application/json')
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
+    return response
 
 if __name__ == '__main__':
     # Start up the Flask app server.
